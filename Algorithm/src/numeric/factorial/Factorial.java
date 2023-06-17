47348650e91896a1eb604ead9a2c746f036e0d17
Ekrem Şallı <111192078+ekremsalli@users.noreply.github.com>
2023-06-17 15:25:08 +0300

    Update
    Update

---
+++ Algorithm/src/numeric/factorial/Factorial.java
@@ -0,0 +1,11 @@
+package numeric.factorial;
+
+public class Factorial {
+
+    public static int factrorial(int a){
+        int total = 1;
+        for(int i=1;i<=a;total *= i++);
+
+        return total;
+    }
+}
---
+++ Algorithm/src/numeric/factorial/FactorialTest.java
@@ -0,0 +1,10 @@
+package numeric.factorial;
+
+public class FactorialTest {
+    public static void main(String[] args) {
+        int a=5;
+
+        System.out.println(Factorial.factrorial(a));
+    }
+
+}
