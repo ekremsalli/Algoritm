c9cdc13d79a8f0f788a497916bd2e9caf85625d7
Ekrem Şallı <111192078+ekremsalli@users.noreply.github.com>
2023-06-17 15:17:49 +0300

    Update
    updt

---
+++ Algorithm/src/numeric/pow/PowTest.java
@@ -0,0 +1,11 @@
+package numeric.pow;
+
+public class PowTest {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 2;
+
+        System.out.println(Pow.pow(a,b));
+
+    }
+}
