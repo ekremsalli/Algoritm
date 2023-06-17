9ddb7b72bb767b4fa2600b56494ee0339f2ef84c
Ekrem Şallı <111192078+ekremsalli@users.noreply.github.com>
2023-06-17 15:37:44 +0300

    up
    up

--- Algorithm/src/palindrome/Palindrome.java
+++ Algorithm/src/palindrome/Palindrome.java
@@ -2,4 +2,23 @@ package palindrome;
 
 public class Palindrome {
 
+    public static boolean palindrome(String s){
+        String reversed="";
+        for(int i=0;i<s.length();i++){
+            reversed += s.charAt(s.length()-i-1);
+        }
+
+        return s.equals(reversed);
+    }
+
+    public static boolean palindrome(int a){
+        String s = a + "";
+        String reversed="";
+        for(int i=0;i<s.length();i++){
+            reversed += s.charAt(s.length()-i-1);
+        }
+
+        return s.equals(reversed);
+    }
+
 }
--- Algorithm/src/palindrome/PalindromeTest.java
+++ Algorithm/src/palindrome/PalindromeTest.java
@@ -1,5 +1,11 @@
 package palindrome;
 
 public class PalindromeTest {
+    public static void main(String[] args) {
+        String s = "eyedipadanadapideye";
+
+        System.out.println(Palindrome.palindrome(s));
+    }
+
 
 }
