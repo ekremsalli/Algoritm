aec2f22e74dc425912c875dfbf5d6cd99c495af7
Ekrem Şallı <111192078+ekremsalli@users.noreply.github.com>
2023-06-19 16:35:56 +0300

    up
    up

--- Algorithm/src/array/fill/ArrayFill.java
+++ Algorithm/src/array/fill/ArrayFill.java
@@ -7,7 +7,16 @@ public class ArrayFill {
     public static void arrayFillRadom(int array[], Random r){
         int idx=0;
         for(int a:array){
-            a = r.nextInt(1,20);
+            a = r.nextInt(1,150);
+            array[idx] = a;
+            idx++;
+        }
+    }
+
+    public static void arrayFillRadom(int array[], Random r,int origin,int bound){
+        int idx=0;
+        for(int a:array){
+            a = r.nextInt(origin,bound);
             array[idx] = a;
             idx++;
         }
--- Algorithm/src/search/linear/LinearSearchTest.java
+++ Algorithm/src/search/linear/LinearSearchTest.java
@@ -2,6 +2,7 @@ package search.linear;
 
 import array.fill.ArrayFill;
 import array.print.ArrayPrint;
+import array.search.ArrayUtilSearch;
 
 import java.util.Random;
 
@@ -12,7 +13,14 @@ public class LinearSearchTest {
         ArrayFill.arrayFillRadom(array,r);
         ArrayPrint.print(array);
         System.out.println();
-        System.out.println(LinearSearch.linearSearch(array,5));
+        //System.out.println(LinearSearch.linearSearch(array,5));
+        System.out.println(ArrayUtilSearch.largestNumber(array));
+        System.out.println(ArrayUtilSearch.secondLargestNumber(array));
+
+        int[] array2 = {1,2,3,4,5,6,6,7,7,8,8,8};
+
+        System.out.println(ArrayUtilSearch.largestNumber(array2));
+        System.out.println(ArrayUtilSearch.secondLargestNumber(array2));
 
     }
 }
