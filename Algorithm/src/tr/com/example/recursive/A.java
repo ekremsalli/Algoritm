da8efd38fc8c999c6aea3def1be6ca336dd688c4
Ekrem Şallı <111192078+ekremsalli@users.noreply.github.com>
2023-06-25 03:19:52 +0300

    u
    u

---
+++ Algorithm/src/tr/com/example/recursive/A.java
@@ -0,0 +1,4 @@
+package tr.com.example.recursive;
+
+class A {
+}
--- Algorithm/src/tr/com/example/unicode/UnicodeExample.java
+++ Algorithm/src/tr/com/example/unicode/UnicodeExample.java
@@ -1,10 +1,12 @@
 package tr.com.example.unicode;
-
+import static java.lang.Math.PI;
 public class UnicodeExample {
     public static void main(String[] args) {
         char ch ='A';
         int unicode = Character.getNumericValue(ch);
         System.out.println("Karakterin Unicode değeri: " + unicode);
         System.out.println("Karakterin Unicode şekli: \\u" + Integer.toHexString(unicode));
+
+        double x = PI;
     }
 }
